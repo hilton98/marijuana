198ee715d53ad5fe89e29e00a40ac9eb06598a68
HiltonDosSantos <hiltoncosta@ecodots.com.br>
2023-12-29 18:52:03 -0300

    feat: simples paginação

--- src/app/(profile-routes)/profile/page.tsx
+++ src/app/(profile-routes)/profile/page.tsx
@@ -5,15 +5,29 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import ganjaData from "../../../../data/ganjaData.json"
-import { Box } from "@mui/material";
+import { Box, Pagination, Stack } from "@mui/material";
+import React from 'react';
 
 
 export default function Profile(){
+    const totalItems = ganjaData.length;
+    const itemsPerPage = 6
+    const [initialIndex, setInitialIndex] = React.useState(0);
+    const [lastIndex, setlastIndex] = React.useState(itemsPerPage-1);
+    const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        let base = (value-1)* itemsPerPage
+        let roof = value * itemsPerPage
+        setInitialIndex(base)
+        setlastIndex(roof)
+    };
+    
     return (
         <>
         <Box sx={{
             display: "flex",
             justifyContent: "center",
+            paddingTop: "3.5rem",
+            paddingBottom: "3.5rem"
         }}>
             <Box sx={{
                 width: 1000,
@@ -21,9 +35,10 @@ export default function Profile(){
                 gridTemplateColumns: "auto auto auto",
                 gap: 2,
                 padding: 2,
-                paddingBottom: "3.5rem",
             }}>
-                {ganjaData.map( specificMarijuana => (
+                {ganjaData.sort((a, b) => (a.name > b.name) ? 1 : -1)
+                .slice(initialIndex, lastIndex)
+                .map( specificMarijuana => (
                     <Card sx={{ maxWidth: 345 }}>
                         <CardMedia
                             sx={{ height: 140 }}
@@ -42,6 +57,16 @@ export default function Profile(){
                 ))}
             </Box>
         </Box>
+        <Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            paddingBottom: "4rem"
+        }}>
+            <Stack spacing={2}>
+                <Pagination count={Math.round(totalItems/itemsPerPage)} defaultPage={1} shape="rounded" onChange={handleChange}/>
+            </Stack>
+        </Box>
+
         </>
     )
 }
